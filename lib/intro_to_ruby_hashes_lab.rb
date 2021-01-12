c64d98613d36158a192bfc48435bf0fa7bf1cb38
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-12 18:16:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,9 +6,9 @@ my_hash_creator(:name, "Grace Hopper")
 
 def read_from_hash(hash, key)
    hash_to_read = {:name=>"Steve", :age=>31}
+   hash_to_read[]
 end
 
-read_from_hash(hash_to_read)
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
