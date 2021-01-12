20c1a56d79dbf679f7f642800288efc3558fe976
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-12 18:05:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def my_hash_creator(key, value)
-  {:name => "Donovan", :1=>2}
+  {key=>value}
 end
 
-
+my_hash_creator(:name, "Grace Hopper")
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
